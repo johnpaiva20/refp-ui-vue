2b8e501259fcc11647b0a3934fa9caf44cb32289
Leonardo Evangelista <leonardodaraujo@hotmail.com>
2019-05-18 07:11:39 -0300

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
+
+import Vuetify from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
+import 'vuetify/dist/vuetify.min.css' 
+Vue.use(Vuetify, {
+  iconfont: 'md',
+})
+
+
 Vue.config.productionTip = false
 
 new Vue({
